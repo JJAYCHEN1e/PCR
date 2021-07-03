0014e926c0e7c647bf57a524b9a0fee58b9e01de
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2021-07-03 21:40:44 +0800

    Adding Debug.Log for UnityToast in non-ios platforms.

--- Assets/Plugins/UnityToast/UnityToast.cs
+++ Assets/Plugins/UnityToast/UnityToast.cs
@@ -5,34 +5,47 @@ using System.Runtime.InteropServices;
 
 public class UnityToast : MonoBehaviour
 {
-    public static void InitBotEmojiView(){
+    public static void InitBotEmojiView()
+    {
 #if UNITY_IOS && !UNITY_EDITOR
         _initBotEmojiView();
 #endif
     }
-    
-    public static void ShowBotToast(string text, float duration){
+
+    public static void ShowBotToast(string text, float duration)
+    {
 #if UNITY_IOS && !UNITY_EDITOR
         _showBotToast(text, duration);
+#else
+        Debug.Log("Showing Bot Toast: " + text);
 #endif
     }
-    
-    public static void ShowTopToast(string text, float duration = 5.0f){
+
+    public static void ShowTopToast(string text, float duration = 5.0f)
+    {
 #if UNITY_IOS && !UNITY_EDITOR
         _showTopToast(text, duration);
+#else
+        Debug.Log("Showing Top Toast: " + text);
 #endif
     }
-    
-    
-    public static void ShowBottomToast(string text, float duration = 5.0f){
+
+
+    public static void ShowBottomToast(string text, float duration = 5.0f)
+    {
 #if UNITY_IOS && !UNITY_EDITOR
         _showBottomToast(text, duration);
+#else
+        Debug.Log("Showing Bottom Toast: " + text);
 #endif
     }
-    
-    public static void ShowAlert(string title, string message){
+
+    public static void ShowAlert(string title, string message)
+    {
 #if UNITY_IOS && !UNITY_EDITOR
         _showAlert(title, message);
+#else
+        Debug.Log("Showing Alter Toast: " + title + " - " + message);
 #endif
     }
 
@@ -40,22 +53,22 @@ public class UnityToast : MonoBehaviour
     [DllImport("__Internal")]
     private static extern void _initBotEmojiView();
 #endif
-    
+
 #if UNITY_IOS && !UNITY_EDITOR
     [DllImport("__Internal")]
     private static extern void _showBotToast(string text, float duration);
 #endif
-    
+
 #if UNITY_IOS && !UNITY_EDITOR
     [DllImport("__Internal")]
     private static extern void _showTopToast(string text, float duration);
 #endif
-    
+
 #if UNITY_IOS && !UNITY_EDITOR
     [DllImport("__Internal")]
     private static extern void _showBottomToast(string text, float duration);
 #endif
-    
+
 #if UNITY_IOS && !UNITY_EDITOR
     [DllImport("__Internal")]
     private static extern void _showAlert(string title, string message);
