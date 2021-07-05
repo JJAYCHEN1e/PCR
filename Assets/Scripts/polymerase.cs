954c13e4c773068b293bec80240464b0133265f9
10165101109 <10165101109@stu.ecnu.edu.cn>
2021-07-05 20:43:21 +0800

    循环时间缩短&盖子，移液器恢复原位

--- Assets/Scripts/polymerase.cs
+++ Assets/Scripts/polymerase.cs
@@ -19,10 +19,6 @@ public class polymerase : MonoBehaviour
         m = GameObject.Find("10").transform;
         l = GameObject.Find("100").transform;
         hat = GameObject.Find("Taq酶试剂盖").transform;
-        originSpos = s.position;
-        originMpos = m.position;
-        originLpos = l.position;
-        originHatpos = hat.position;
     }
 
     // Update is called once per frame
@@ -67,11 +63,11 @@ public class polymerase : MonoBehaviour
             se.Append(s.DOMove(GameObject.Find("酶上方").transform.position ,2f));
             se.Append(s.DOLocalRotate(new Vector3(-20f, 0f, 0f), 1f, RotateMode.WorldAxisAdd));
             se.Append(s.DOMove(GameObject.Find("试管内").transform.position,2f));
-            se.Append(hat.DOMove(originHatpos ,2f));
+            se.Append(hat.DOMove(GameObject.Find("Taq酶备份盖").transform.position ,2f));
             DOTween.To(() => timer, a => timer = a, 1, 18f).OnComplete(() => SpeechController.Speak("废弃枪头"));
             se.Append(s.DOMove(GameObject.Find("垃圾桶上").transform.position,2f));
             se.Append(s.DOMove(GameObject.Find("垃圾桶内").transform.position ,2f));
-            se.Append(s.DOMove(originSpos,1.5f));  
+            se.Append(s.DOMove(GameObject.Find("2.5备份").transform.position,1.5f));  
         } 
         // else if(m.position == originMpos){
         //     type = "10";
